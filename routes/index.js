9bc8e47c315d8e206eab4e30eae08cfe5ede07d8
k.denisov <k.denisov@goodt.me>
2019-12-03 23:30:17 +0300

    Little fixes

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,32 @@ const jwt = require('jsonwebtoken');
 router.use(async (req, res, next) => {
 
     let token = req.cookies['session'],
-        userData = decodeToken(token);
+        userData = getUserData(token);
 
     if (!userData) {
         res.clearCookie("session");
         return res.status(401).send({ message: 'You are not authenticated' })
     }
 
-    userData.permissions = permissions[userData.role] || [];
-
     req.userData = userData;
 
     next();
 });
 
-
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+function getUserData(token) {
+    let userData = decodeToken(token);
+
+    if (!userData)
+        return null;
+
+    userData.permissions = permissions[userData.role] || [];
+
+    return userData;
+}
 
 function decodeToken(token) {
     const secret = 'secret';
@@ -32,7 +40,7 @@ function decodeToken(token) {
     try {
         return jwt.verify(token, secret);
     } catch (e) {
-        console.log(e)
+        console.log(e);
         return null;
     }
 }
